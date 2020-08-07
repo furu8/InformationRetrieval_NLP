9a009f20ef2cf7226021fd28eaae440d084531b5
furu8 <foooolurk1125@gmail.com>
2020-08-07 17:52:34 +0900

    空白文字削除の前位処理

--- script/task1-3.py
+++ script/task1-3.py
@@ -8,13 +8,29 @@ def read_txt(path, isstopword):
         df = df.rename(columns={'#http://www.textfixer.com/resources/common-english-words.txt': 'stop_word'})
     else:
         df = pd.read_csv(path, sep='\t', names=('word', 'count'))
+        
     return df
+
+def remove_stop_word(df, df_stop):
+    # 前処理
+    word_list = [word.replace(' ', '') for word in df['word'].values.flatten()]
+    stop_word_list = df_stop['stop_word'].values.flatten()
+    
+    # ストップワードの単語を除去
+    new_word_list = [word for word in word_list if word not in stop_word_list]
+    
+    return new_word_list
 
 def main():
     # 読み込み
     df = read_txt('../data_file/interim/task1-2_result.txt', False)
     df_stop = read_txt('../data_file/raw/stop_words_list(UTF-8).txt', True)
-    print(df_stop)
+    
+    # 除去
+    new_word_list = remove_stop_word(df, df_stop)
+
+    # 出力
+
     
 if __name__ == "__main__":
     main()
