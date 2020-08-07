720f2b2771d51df57b2d42b213af4cb1f9b79be1
furu8 <foooolurk1125@gmail.com>
2020-08-07 16:58:13 +0900

    単語出現数計算

--- script/task1-2.py
+++ script/task1-2.py
@@ -5,11 +5,25 @@ def read_txt(path):
     df = pd.read_csv(path, sep='\n', names=('word',))
     return df
 
+def calc_word_count(df):
+    # カウント
+    df_count = df['word'].value_counts() 
+    df_count = df_count.reset_index(drop=False)
+    df_count = df_count.rename(columns={'index': 'word', 'word':'count'}) # カラム名変更
+
+    return df_count
+
 def main():
     # 読み込み
     df = read_txt('../data_file/interim/task1-1_result.txt')
 
-    print(df)
+    # カウント数出力
+    
+    print(df_count)
+    
+
+    # for c in df_count.values:
+    #     print(c)
 
 if __name__ == "__main__":
     main()
