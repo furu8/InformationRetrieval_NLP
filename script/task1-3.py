47d7f451b960743e6775faefb7be664b542d8c95
furu8 <foooolurk1125@gmail.com>
2020-08-07 20:23:15 +0900

    finish task1-3

--- script/task1-3.py
+++ script/task1-3.py
@@ -11,11 +11,7 @@ def read_txt(path, isstopword):
         
     return df
 
-def remove_stop_word(df, df_stop):
-    # 前処理
-    word_list = [word.replace(' ', '') for word in df['word'].values.flatten()]
-    stop_word_list = df_stop['stop_word'].values.flatten()
-    
+def remove_stop_word(word_list, stop_word_list):
     # ストップワードの単語を除去
     new_word_list = [word for word in word_list if word not in stop_word_list]
     
@@ -26,17 +22,17 @@ def main():
     df = read_txt('../data_file/interim/task1-2_result.txt', False)
     df_stop = read_txt('../data_file/raw/stop_words_list(UTF-8).txt', True)
     
+    # 前処理
+    word_list = [word.replace(' ', '') for word in df['word'].values.flatten()]
+    df['word'] = word_list
+    stop_word_list = df_stop['stop_word'].values.flatten()
+
     # 除去
-    new_word_list = remove_stop_word(df, df_stop)
+    new_word_list = remove_stop_word(word_list, stop_word_list)
 
     # 出力
-    new_df = pd.DataFrame(new_word_list, columns=[['word']])
-    df = df.sort_values('word')
-    print(df)
-    new_df = pd.merge(new_df, df, how='outer')
-    
-    print(new_df)
-    # [print(word) for word in new_word_list] 
-    
+    for word in df.values:
+        [print('{:<15}\t {}'.format(word[0], word[1])) for new_word in new_word_list if word[0] == new_word] # brokenheartedlyが15文字で最大    
+
 if __name__ == "__main__":
     main()
