e2120c9efce30920f5342aa73d6d03d3022ee1e3
furu8 <foooolurk1125@gmail.com>
2020-08-07 15:47:37 +0900

    リストの中を文字に変換

--- script/task1-1.py
+++ script/task1-1.py
@@ -1,4 +1,5 @@
 import pandas as pd
+import numpy as np
 
 def read_txt(path):
     df = pd.read_csv(path, sep='\n')
@@ -6,13 +7,39 @@ def read_txt(path):
     return df
 
 def pre_lower_word(doc_list):
+    """
+    文書をすべて小文字に変換
+    """
+    # 小文字変換
+    doc_lower_str_list = str(doc_list[0]).lower() # listは0番目しかない
+    # 比較
+    # print(doc_list)
+    # print(doc_lower_str_list)
+
+    return doc_lower_str_list
+
+def pre_replace_word(doc_list, symbol_list):
+    """
+    文書のsymbol_listの記号と空白文字を\nに置き換え
+    """
     doc_str_list = str(doc_list)
-    return doc_str_list.lower()
+    doc_sym_list = [doc_str_list.replace(sym, '\n') for sym in symbol_list]
+    print(doc_sym_list)
+
 
 def preprocessing_word(df):
-    doc_list = df.values
-    lower_list = [pre_lower_word(doc) for doc in doc_list]
-    print(lower_list)
+    """
+    前処理をほどこす
+    """
+    doc_list = df.values # 二次元リスト
+    print([pre_lower_word(doc) for doc in doc_list])
+    # doc_lower_list = [pre_lower_word(doc) for doc in doc_list]
+    # print(np.array(doc_lower_list).shape)
+    # print(doc_lower_list)
+    symbol_list = ['(', ')', '"', '-', ' ']
+    # doc_sym_list = [pre_replace_word(doc, symbol_list) for doc in doc_lower_list]
+
+
 
 def main():
     df = read_txt('../data_file/raw/doc.txt')
