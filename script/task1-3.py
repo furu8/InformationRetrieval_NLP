4f2257d1b35c650777ae275cb88d79320664778e
furu8 <foooolurk1125@gmail.com>
2020-08-07 17:30:26 +0900

    read stop word

--- script/task1-3.py
+++ script/task1-3.py
@@ -1,14 +1,20 @@
 import pandas as pd
 import numpy as np
 
-def read_txt(path):
-    df = pd.read_csv(path, sep='\t', names=('word', 'count'))
+def read_txt(path, isstopword):
+    # stopwordかどうか
+    if isstopword:
+        df = pd.read_csv(path, sep='\n')
+        df = df.rename(columns={'#http://www.textfixer.com/resources/common-english-words.txt': 'stop_word'})
+    else:
+        df = pd.read_csv(path, sep='\t', names=('word', 'count'))
     return df
 
 def main():
     # 読み込み
-    df = read_txt('../data_file/interim/task1-2_result.txt')
-    print(df)
+    df = read_txt('../data_file/interim/task1-2_result.txt', False)
+    df_stop = read_txt('../data_file/raw/stop_words_list(UTF-8).txt', True)
+    print(df_stop)
     
 if __name__ == "__main__":
     main()
