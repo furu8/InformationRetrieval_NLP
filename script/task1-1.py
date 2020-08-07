632f6a4a1034a02c4407816dd05ca8a6efab7f28
furu8 <foooolurk1125@gmail.com>
2020-08-07 15:03:27 +0900

    test lower word

--- script/task1-1.py
+++ script/task1-1.py
@@ -5,9 +5,18 @@ def read_txt(path):
     df = df.rename(columns={'#http://disney.wikia.com/wiki/Frozen': 'doc'})
     return df
 
+def pre_lower_word(doc_list):
+    doc_str_list = str(doc_list)
+    return doc_str_list.lower()
+
+def preprocessing_word(df):
+    doc_list = df.values
+    lower_list = [pre_lower_word(doc) for doc in doc_list]
+    print(lower_list)
+
 def main():
     df = read_txt('../data_file/raw/doc.txt')
-
+    preprocessing_word(df)
 
 if __name__ == "__main__":
     main()
