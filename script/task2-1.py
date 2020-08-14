62433b0b82ebb06b4ba1f7e239a7d0b35796b083
furu8 <foooolurk1125@gmail.com>
2020-08-14 13:13:01 +0900

    雑な前処理完了

--- script/task2-1.py
+++ script/task2-1.py
@@ -3,13 +3,13 @@ import numpy as np
 import re
 
 def print_preprocessing_debug(doc_list):
-    print('# httpsデバッグ') # httpsデバッグ
+    print('# httpsデバッグ') # https
     print(doc_list[45989])
     print(re.sub(r'https?://[\w/:%#\$&\?\(\)~\.=\+\-…]+', "", doc_list[45989]))
 
-    print('#半角記号,数字,英字') #半角記号,数字,英字
-    print(doc_list[6])
-    print(re.sub(r'[~`!@#$%^&*()\-+={}|.,<>/?:;"[\]\'\\]', "", doc_list[6]))
+    print('#半角記号') #半角記号
+    print(doc_list[6977])
+    print(re.sub(r'[~`!@#$%^&*()\-━+={}|.,<>/?:;"[\]\'\\]', "", doc_list[6977]))
 
     print('# 全角記号') # 全角記号
     print(doc_list[18309])
@@ -18,6 +18,14 @@ def print_preprocessing_debug(doc_list):
     print("# [’]を[']に置換") # [’]を[']に置換
     print(doc_list[15])
     print(re.sub(r'[‘’]', "'", doc_list[15]))
+
+    print('# 大文字小文字変換') # 大文字小文字変換
+    print(doc_list[2])
+    print(doc_list[2].lower())
+
+    print('# \\n削除') # \\n削除
+    print(doc_list[2])
+    print(re.sub(r'[\n]', "", doc_list[2]))
 
 def read_txt(path):
     with open(path, 'r') as f:
@@ -29,18 +37,45 @@ def read_txt(path):
     
     return title_list, doc_list
 
+def remove_symbol_word(doc):
+    # https
+    new_doc = re.sub(r'https?://[\w/:%#\$&\?\(\)~\.=\+\-…]+', "", doc)
+
+    # \\n削除
+    new_doc = re.sub(r'[\n]', "", new_doc)
+
+    # # [‘]と[’]を[']に置換
+    # new_doc = (re.sub(r'[‘’]', "'", new_doc) # 置換すればpeople’sがpeople'sとして扱えるが、'mental enslavement'などが残る
+    
+    # 半角記号
+    new_doc = re.sub(r'[~`!@#$%^&*()\-━+={}|.,<>/?:;"[\]\'\\]', "", new_doc)
+    
+    # 全角記号
+    new_doc = re.sub(r'[︰”「」@]', "", new_doc)
+
+    return new_doc
+
+def preprocessing_word(doc_list):
+    """
+    前処理をほどこす
+    """
+    # 小文字変換
+    doc_lower_list = [doc.lower() for doc in doc_list]
+    
+    # 記号除去
+    doc_sym_list = [remove_symbol_word(doc) for doc in doc_lower_list]
+
+    return doc_sym_list
+
 def main():
     # テキストから記事のタイトルと内容を取得
     titile_list, doc_list = read_txt('../data_file/raw/doc_set.txt')
 
-    # 前処理debug
-    print_preprocessing_debug(doc_list) 
-
     # 前処理
-    # doc_list = [doc for doc in doc_list if doc != '\n'] # 改行文字しかない要素を除外
-    
-    
-
+    # print_preprocessing_debug(doc_list) # 前処理確認デバッグ
+    doc_list = [doc for doc in doc_list if doc != '\n'] # 改行文字しかない要素を除外
+    new_doc_list = preprocessing_word(doc_list)
+        
 
 if __name__ == "__main__":
     main()
